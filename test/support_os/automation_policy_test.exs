346708e63fcc0435c150aec0ef9ff977abcd2c3b
szTheory <szTheory@users.noreply.github.com>
2026-05-10 14:05:57 -0400

    test(M002-S03-01): add failing test for DefaultAutomationPolicy

---
+++ test/support_os/automation_policy_test.exs
@@ -0,0 +1,14 @@
+defmodule SupportOS.AutomationPolicyTest do
+  use ExUnit.Case, async: true
+
+  alias SupportOS.DefaultAutomationPolicy
+
+  describe "DefaultAutomationPolicy" do
+    test "decide/2 returns :draft_only for any proposal" do
+      proposal = %{content: "This is a proposal", conversation_id: "conv_123"}
+      opts = %{}
+      
+      assert :draft_only == DefaultAutomationPolicy.decide(proposal, opts)
+    end
+  end
+end
