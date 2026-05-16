6695534daee35a0ab9d2fc530794a3d3c3bc163e
szTheory <szTheory@users.noreply.github.com>
2026-05-16 21:55:30 +0200

    feat(M007-S01-01): attach telemetry handler for Scrypath ingestion

--- lib/cairnloop/application.ex
+++ lib/cairnloop/application.ex
@@ -7,6 +7,8 @@ defmodule Cairnloop.Application do
 
   @impl true
   def start(_type, _args) do
+    attach_telemetry()
+
     children = [
       # Starts a worker by calling: Cairnloop.Worker.start_link(arg)
       # {Cairnloop.Worker, arg}
@@ -16,5 +18,30 @@ defmodule Cairnloop.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Cairnloop.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp attach_telemetry do
+    :telemetry.attach(
+      "cairnloop-conversation-resolved-scrypath",
+      [:cairnloop, :conversation, :resolved],
+      &__MODULE__.handle_conversation_resolved/4,
+      nil
+    )
+  end
+
+  @doc false
+  def handle_conversation_resolved(_event, _measurements, metadata, _config) do
+    job =
+      %{
+        "conversation_id" => metadata[:conversation_id] || metadata[:id],
+        "text" => metadata[:text] || ""
+      }
+      |> Cairnloop.Workers.IngestScrypath.new()
+
+    try do
+      Oban.insert(job)
+    rescue
+      _ -> :ok
+    end
   end
 end
