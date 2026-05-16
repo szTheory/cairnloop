02e9ee71e3e18fbb30c6a21e99dff7ba749d5827
szTheory <szTheory@users.noreply.github.com>
2026-05-16 23:15:41 +0200

    test(02-01): add failing test for Notifier behaviour

--- test/cairnloop/notifier_test.exs
+++ test/cairnloop/notifier_test.exs
@@ -1,9 +1,9 @@
 defmodule Cairnloop.NotifierTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "defines expected callbacks" do
+  test "callbacks are defined" do
+    # We can check if the behaviour has the required callbacks
     callbacks = Cairnloop.Notifier.behaviour_info(:callbacks)
-    
     assert {:on_conversation_resolved, 2} in callbacks
     assert {:on_sla_breach, 3} in callbacks
   end
