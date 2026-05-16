bdd6329c928353f37f8711b0c1b30d51f86b2d6b
szTheory <szTheory@users.noreply.github.com>
2026-05-17 00:30:57 +0200

    test(02-01): add test for Chimeway adapter
    - Validates Notifier behaviour implementation
    - Validates on_sla_breach and on_conversation_resolved presence

--- test/cairnloop/notifier/chimeway_test.exs
+++ test/cairnloop/notifier/chimeway_test.exs
@@ -1,27 +1,20 @@
 defmodule Cairnloop.Notifier.ChimewayTest do
   use ExUnit.Case, async: true
 
-  alias Cairnloop.Notifier.Chimeway, as: ChimewayNotifier
-
-  test "implements Cairnloop.Notifier behaviour" do
-    assert :ok = ChimewayNotifier.on_conversation_resolved(%{id: "conv_123"}, %{})
-  end
-
-  test "on_sla_breach/3 triggers Chimeway SLABreachNotifier" do
-    conversation = %{id: "conv_123", account_id: "acc_1"}
-    sla = %{target_type: "first_response", completed_at: ~U[2023-01-01 12:00:00Z]}
+  # We don't have Chimeway fully loaded or mocked yet, but we can verify it implements the behaviour.
+  # For TDD RED, this will fail because Cairnloop.Notifier.Chimeway does not exist.
+  
+  describe "Cairnloop.Notifier.Chimeway" do
+    test "implements Notifier behaviour" do
+      assert Cairnloop.Notifier.Chimeway.__info__(:attributes)[:behaviour] == [Cairnloop.Notifier]
+    end
     
-    # Check that on_sla_breach doesn't crash. In a real scenario with Chimeway missing
-    # DB config, it returns {:error, _} or similar, or raises if it hits Repo directly.
-    # Actually, let's just make sure it's callable.
-    try do
-      ChimewayNotifier.on_sla_breach(conversation, sla, %{})
-      assert true
-    rescue
-      _e -> 
-        # If it raises due to no DB connection, that's fine for this unit test 
-        # since we don't mock the Repo.
-        assert true
+    test "defines on_sla_breach/3" do
+      assert {:on_sla_breach, 3} in Cairnloop.Notifier.Chimeway.__info__(:functions)
+    end
+    
+    test "defines on_conversation_resolved/2" do
+      assert {:on_conversation_resolved, 2} in Cairnloop.Notifier.Chimeway.__info__(:functions)
     end
   end
 end
