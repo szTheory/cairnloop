a473d67d4b05cdfe05cdf2531a59ba271e383a0a
szTheory <szTheory@users.noreply.github.com>
2026-05-17 00:30:34 +0200

    test(02-01): add test for Notifier behaviour
    - Validates on_conversation_resolved/2 exists
    - Validates on_sla_breach/3 exists

--- test/cairnloop/notifier_test.exs
+++ test/cairnloop/notifier_test.exs
@@ -1,10 +1,17 @@
 defmodule Cairnloop.NotifierTest do
   use ExUnit.Case, async: true
 
-  test "defines expected callbacks" do
-    callbacks = Cairnloop.Notifier.behaviour_info(:callbacks)
-    
-    assert Keyword.has_key?(callbacks, :on_conversation_resolved)
-    assert Keyword.has_key?(callbacks, :on_sla_breach)
+  describe "Cairnloop.Notifier behaviour" do
+    test "defines required callbacks" do
+      # In Elixir, we can check if a module defines a behaviour by checking its callbacks
+      # However, since it's just a behaviour, the compiler checks the implementers.
+      # To test the behaviour itself, we can define a dummy module and ensure it complains
+      # if it's missing the callbacks, but the easiest way to assert the behaviour exists
+      # is to just check its module info or manually implement it.
+      
+      # Let's ensure the callbacks are defined
+      assert {:on_conversation_resolved, 2} in Cairnloop.Notifier.behaviour_info(:callbacks)
+      assert {:on_sla_breach, 3} in Cairnloop.Notifier.behaviour_info(:callbacks)
+    end
   end
 end
